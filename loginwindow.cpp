ce88a76c817d7164efc2fd103f33319dc0fe9e27
Murtuza Rahman <murtuza.rahman83@gmail.com>
2023-03-21 16:03:38 +0600

    login

--- loginwindow.cpp
+++ loginwindow.cpp
@@ -4,12 +4,19 @@
 #include <QSystemTrayIcon>
 #include <QJsonObject>
 #include <QJsonDocument>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QDebug>
+#include "mainwindow.h"
 
 LoginWindow::LoginWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::LoginWindow)
 {
     ui->setupUi(this);
+    ui->leUserId->setText("105418");
+    ui->lePassword->setText("123456");
 }
 
 LoginWindow::~LoginWindow()
@@ -21,38 +28,46 @@ void LoginWindow::on_pushButton_clicked()
 {
   QString leUserId = ui->leUserId->text();
   QString lePassword = ui->lePassword->text();
-
   if(leUserId.length() == 0 || lePassword.length() == 0) {
       QMessageBox::warning(this, "Login","Insert all information");
   } else {
-      qDebug() << "clicked: ";
-    RestApi api;
-    QJsonObject data;
-    data["pin"] = "105418";
-    data["password"] = "123456";
-    QJsonDocument doc(data);
-    QByteArray postData = doc.toJson();
-    QUrl url = QUrl::fromUserInput("https://hub.sensor.buzz/upg-auth/api/v1/account/login");
-    api.post(url, postData);
-
-    connect(&api, &RestApi::success, this, &LoginWindow::onReplyReceived);
+      this->makeRequest(leUserId,lePassword);
   }
 }
 
-void LoginWindow::onReplyReceived(QByteArray response)
-{
-    qDebug() << "Response: ";
+
+void LoginWindow::makeRequest(QString leUserId, QString lePassword) {
+    QByteArray postData;
+        postData.append("pin=");
+        postData.append(leUserId.toUtf8());
+        postData.append("&password=");
+        postData.append(lePassword.toUtf8());
+
+    QNetworkRequest request(QUrl("https://hub.sensor.buzz/upg-auth/api/v1/account/login"));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+
+    QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+    QNetworkReply *reply = manager->post(request, postData);
+    // Connect the reply's finished() signal to a slot that will handle the response
+    connect(reply, SIGNAL(finished()), this, SLOT(handleResponse()));
 }
 
-//void LoginWindow::onReplyReceived(QNetworkReply *reply)
-//{
-//    qDebug() << "Response: ";
-//    if (reply->error() != QNetworkReply::NoError) {
-//        qDebug() << "Error: " << reply->errorString();
-//        return;
-//    }
+void LoginWindow::handleResponse() {
+    // Get the HTTP reply from the sender object
+    QNetworkReply *reply = qobject_cast<QNetworkReply *>(sender());
 
-//    QByteArray data = reply->readAll();
-//    // Do something with the response data
-//}
+    if (reply->error() == QNetworkReply::NoError) {
+        QByteArray data = reply->readAll();
+        qDebug() << data;
+
+        MainWindow *main = new MainWindow(this);
+            main->show();
+            this->hide();
+    } else {
+        qDebug() << "Error: " << reply->errorString();
+    }
+
+    // Clean up the reply object
+    reply->deleteLater();
+}
 
--- loginwindow.h
+++ loginwindow.h
@@ -2,7 +2,7 @@
 #define LOGINWINDOW_H
 
 #include <QMainWindow>
-#include "restapi.h"
+//#include "restapi.h"
 
 namespace Ui {
 class LoginWindow;
@@ -18,7 +18,9 @@ public:
 
 private slots:
     void on_pushButton_clicked();
-    void onReplyReceived(QByteArray response);
+//    void onReplyReceived(QByteArray response);
+    void makeRequest(QString leUserId, QString lePassword);
+    void handleResponse();
 
 private:
     Ui::LoginWindow *ui;
